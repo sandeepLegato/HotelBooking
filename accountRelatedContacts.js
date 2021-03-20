32a02400f47bced5fc6316daa624521a87e23b5e
sandeepLegato <81031253+sandeepLegato@users.noreply.github.com>
2021-03-21 01:01:25 +0530

    Add files via upload

---
+++ accountRelatedContacts.js
@@ -0,0 +1,14 @@
+import { LightningElement, wire, api, track } from 'lwc';
+import getContacts from '@salesforce/apex/AccountRelatedContacts.getContacts';
+
+export default class AccountRelatedContacts extends LightningElement {
+    @api recordId;
+    @track searchKey = '';
+    @wire(getContacts, {searchKey : '$searchKey'})
+    contacts;
+
+    connectedCallback(){
+        this.searchKey = this.recordId;
+    }
+
+}
